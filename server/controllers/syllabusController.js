4c5abddb6e1c642d2ae1139e9044478b6be3df8e
DoniZefironi <136065842+DoniZefironi@users.noreply.github.com>
2024-11-26 11:04:10 +0300

    feat: fix bugs

--- server/controllers/syllabusController.js
+++ server/controllers/syllabusController.js
@@ -1,30 +1,46 @@
-const uuid = require('uuid')
-const path = require('path')
-const {Syllabus} = require('../models/models')
-const ApiError = require('../error/ApiError')
+const uuid = require('uuid');
+const path = require('path');
+const { Syllabus } = require('../models/models');
+const ApiError = require('../error/ApiError');
 
 class SyllabusController {
-    async create(req, res) {
+    async create(req, res, next) {
+        try {
             const { date } = req.body;
-            const { syllfile } = req.files
-            let fileName = uuid.v4() + ".png"
-            syllfile.mv(path.resolve(__dirname, '..', 'static', fileName))
-            const syllabus = await Syllabus.create({date, syllfile: fileName})
-            return res.json(syllabus)
+            const { syllfile } = req.files;
 
+            if (!syllfile) {
+                return next(ApiError.badRequest('No file uploaded'));
+            }
+
+            let fileName = uuid.v4() + path.extname(syllfile.name);
+            let filePath = path.resolve(__dirname, '..', 'static', fileName);
+
+            syllfile.mv(filePath, (err) => {
+                if (err) {
+                    return next(ApiError.internal('File upload failed'));
+                }
+            });
+
+            const syllabus = await Syllabus.create({ date, syllfile: fileName });
+            return res.json(syllabus);
+        } catch (err) {
+            return next(ApiError.internal(err.message));
+        }
     }
 
     async getAll(req, res) {
-        
+        const syllabus = await Syllabus.findAll();
+        return res.json(syllabus);
     }
 
     async getOne(req, res) {
-        
+
     }
 
     async deleteOne(req, res) {
-        
+
     }
 }
 
-module.exports = new SyllabusController()
+module.exports = new SyllabusController();
--- server/routes/subjectRouter.js
+++ server/routes/subjectRouter.js
@@ -2,8 +2,8 @@ const Router = require('express')
 const router = new Router()
 const subjectController = require('../controllers/subjectController')
 
-router.post('/', subjectController.create)
-router.get('/', subjectController.getAll) 
+router.post('/create', subjectController.create)
+router.get('/getAll', subjectController.getAll) 
 router.get('/getOne/:id', subjectController.getOne)
 router.delete('/deleteOne/:id', subjectController.deleteOne)
 
